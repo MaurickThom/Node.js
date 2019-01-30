f504234da86897ff572fb6817472b40d222a72f0
UsuarioTemporal <thomtwd@gmail.com>
2019-01-30 18:58:50 -0500

    Analizando node.js

--- 2-ciclo-eventos/app.js
+++ 2-ciclo-eventos/app.js
@@ -4,3 +4,5 @@ const greet = (name)=>{
     let message = `Hello ${name}`
     return message
 }
+let greeting = greet('Thom')
+console.log(greeting)
---
+++ 2-ciclo-eventos/app2.js
@@ -0,0 +1,12 @@
+console.log('Inicio de programa')
+
+setTimeout(() => {
+    console.log('Primer Timeout')
+}, 3000);
+
+setTimeout(() => {
+    console.log('Segundo timeout');
+}, 0);
+setTimeout(() => {
+    console.log('Tercer timeout');
+}, 0);
