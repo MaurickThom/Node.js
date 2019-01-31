f85e2d644777d10ebfaaa65aa5f78ab48999c240
UsuarioTemporal <thomtwd@gmail.com>
2019-01-31 18:02:45 -0500

    Arreglando comentarios

--- 3-modelos-asincronicos/continuadores.js
+++ 3-modelos-asincronicos/continuadores.js
@@ -1,9 +1,14 @@
 // Tambien conocido como callbacks
 
 /**
- * Modelo nativo que utiliza nodejs en sus API's para dar soporte a la programacion asincrona es el paso de continuadores.Cada operacion no bloqueante recibe una funcion como ultimo parametro que incluye la lógica de continuadores que debe ser invocada tras la finalizacion de la misma tanto para procesar los resutlados en caso de exito como para dar los fallos en caso de error.
+ * Modelo nativo que utiliza nodejs en sus API's para dar soporte a la
+ *  programacion asincrona es el paso de continuadores.Cada operacion no bloqueante recibe una funcion como ultimo parametro que incluye la lógica
+ *  de continuadores que debe ser invocada tras la finalizacion de la
+ *  misma tanto para procesar los resutlados en caso de exito como para
+ *  dar los fallos en caso de error.
  * 
- * La funcion de continuacion permite indicar a la operacion bloqueante como debe procesar despues de finalizada la operacion.
+ * La funcion de continuacion permite indicar a la operacion bloqueante
+ *  como debe procesar despues de finalizada la operacion.
  */
 
 //Proveedor
