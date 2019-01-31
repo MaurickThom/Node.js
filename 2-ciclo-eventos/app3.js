fe5bc9b3d2b22b39425b8deab255663015c0e543
UsuarioTemporal <thomtwd@gmail.com>
2019-01-30 21:10:51 -0500

    Ciclo de vida de los procesos en NodeJs

--- 2-ciclo-eventos/app3.js
+++ 2-ciclo-eventos/app3.js
@@ -1,3 +1,12 @@
-const  aFunction = ()=>{
-    
+const  aFunction = (value,callback)=>{
+    console.log(`Value ${value}`)
+    setTimeout(() => {
+        callback(value,Math.pow(value,2)) 
+    }, 0 | Math.random()*100)
 }
+
+aFunction(3,(value,result)=>{
+    console.log(`Value ${value} result ${result}`);
+})
+
+console.log('Terminado?')
