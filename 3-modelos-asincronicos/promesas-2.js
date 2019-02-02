5207d5f66ac97c23ed4bf34d5bc7057fa6062302
UsuarioTemporal <thomtwd@gmail.com>
2019-02-02 15:47:08 -0500

    Primer commit con el cabello corto , modelos es6

--- 3-modelos-asincronicos/promesas-2.js
+++ 3-modelos-asincronicos/promesas-2.js
@@ -9,3 +9,20 @@ myPromise
 .then(number=>number*5)
 .then(number=>console.log(number))
 .catch(err=>console.log(err))
+
+/**
+ * Javascript es un lenguaje de un solo hilo sin interrrupcion .
+ * Asincrono y concurrente .Tiene una pila de ejecucion un bucle de eventos una cola de callback acceso a otras APis
+ */
+let firtsPromise=new Promise((resolve,reject)=>{
+    resolve('Primera promesa')
+})
+let secondPromise=new Promise((resolve,reject)=>{
+    resolve('Segunda pregunta')
+})
+
+firtsPromise.
+then(response=>{
+    return secondPromise
+},err=>console.log(err))
+.then(response=>console.log(response))
