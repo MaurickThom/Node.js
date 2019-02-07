e0a02f9b8357c14a80b3ff9a9729ac1542abb790
UsuarioTemporal <thomtwd@gmail.com>
2019-02-07 17:41:22 -0500

    Terminado usando es6 y buenas practicas

--- extra/debounce_2.js
+++ extra/debounce_2.js
@@ -3,4 +3,26 @@ import words from './palabras.js' ;
 console.log(words)
 const input = document.getElementById('input')
 const content_ = document.getElementById('js-result')
-
+const renderResult = (letter)=>{
+    const result = words
+    .filter(word=>{
+        if(word.toLowerCase().match(`.*${letter.toLowerCase().trim()}.*`)) return word;
+    })
+    .map(word=>`<li>${word}</li>`)
+    .join('')
+    content_.innerHTML=result
+}
+const debounce = (callback,time)=>{
+    let timeoutId
+    if(timeoutId) clearTimeout(timeoutId)
+    return function(){
+        timeoutId=setTimeout(()=>{
+            callback.apply(this,arguments)
+        },time)
+    }
+}
+const triggerContent = debounce(renderResult,100)
+renderResult('')
+input.addEventListener('keyup',e=>{
+    triggerContent(e.target.value)
+})
