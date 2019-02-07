aa66eb50218ac146832c689c72841162448083c2
UsuarioTemporal <thomtwd@gmail.com>
2019-02-07 01:20:03 -0500

    Entendiendo server

--- extra/debounce.js
+++ extra/debounce.js
@@ -1,11 +1,16 @@
-let timeOutId 
+
 const alertar = (nombre,apellido)=>{
-    if(timeOutId) clearTimeout(timeOutId)
     timeOutId = setTimeout(()=>{
         console.log(`${nombre} ${apellido}`)
     },1000)
 }
-
+const debounce=(callback)=>{
+    let timeOutId 
+    return ()=>{
+        if(timeOutId) clearTimeout(timeOutId)
+        const context= this
+    }
+}
 document.getElementById('btn').addEventListener('click',e=>{
     e.preventDefault()
     alertar('Thom','Roman')
