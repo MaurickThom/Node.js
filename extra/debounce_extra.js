4cea632aa79d7846b79f48409cab6d184419fe9d
UsuarioTemporal <thomtwd@gmail.com>
2019-02-07 16:57:51 -0500

    Terminado

--- extra/debounce_extra.js
+++ extra/debounce_extra.js
@@ -1,13 +1,40 @@
-let timeoutId
-const alertar = (nombre, apellido) => {
-	if(timeoutId) clearTimeout(timeoutId)
-	timeoutId=setTimeout(() => {
-		alert(`Mi nombre es ${nombre} ${apellido}`)
-	}, 1000)
+
+// Este método es correcto pero estamos amarrando la tecnica a una funcion
+
+// let timeoutId
+// const alertar = (nombre, apellido) => {
+// 	if(timeoutId) clearTimeout(timeoutId)
+// 	timeoutId=setTimeout(() => {
+// 		alert(`Mi nombre es ${nombre} ${apellido}`)
+// 	}, 1000)
 	
+// }
+// document.getElementById('btn')
+// 	.addEventListener('click', e => {
+// 		e.preventDefault()
+// 		alertar('Thom', 'Roman')
+// 	})
+
+// separando la tecnica de la una funcion
+
+const myAlert=(name,surname)=>{
+	alert(`My name is ${name} ${surname}`)
 }
+
+const debounce = (callback,time)=>{
+	let timeoutId
+	return function(){
+		if(timeoutId) clearTimeout(timeoutId)
+		console.log(this)
+		console.log(arguments)
+		timeoutId=setTimeout(()=>{
+			callback.apply(this,arguments)
+		},time)
+	}
+}
+const triggerAlert = debounce(myAlert,1000)
 document.getElementById('btn')
-	.addEventListener('click', e => {
+	.addEventListener('click',e=>{
 		e.preventDefault()
-		alertar('Thom', 'Roman')
+		triggerAlert('Thom','Roman')
 	})
