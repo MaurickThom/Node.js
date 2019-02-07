572bebd32670c63a6b176399c969d70a5219b264
UsuarioTemporal <thomtwd@gmail.com>
2019-02-07 13:28:23 -0500

    Mas curiosidade

--- extra/debounce.js
+++ extra/debounce.js
@@ -1,5 +1,12 @@
 /**
- * La palabra this se conporta de una manera muy peculiar en javascript, a diferencia de otros lenguajes la palabra this se comporta DE DIFERENTE manera dependiendo del contexto en javascript
+ * La palabra this se conporta de una manera muy peculiar en javascript, a diferencia de otros lenguajes la palabra this se comporta DE DIFERENTE manera dependiendo del contexto en javascript.
+ * 
+ * El valor this está determiandno por cómo se llama a la funcion.No puede ser establecida por una asignacion en tiempo de ejecucion , y esto puede ser diferente cada vez que la funcion es llamada.
+ * 
+ * ES5 introdujo el método bind() para establecer el valor de la funcion this independientemente de como es llamada
+ * 
+ * 
+ * La palabra this dentro de una funcion depende de como es llamada
  */
 const alertar = (nombre,apellido)=>{
     timeOutId = setTimeout(()=>{
