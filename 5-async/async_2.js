cd6bc9578a5b6cba70c383a180bdcf8f01ec5cfc
UsuarioTemporal <thomtwd@gmail.com>
2019-02-08 00:59:42 -0500

    Async

--- 5-async/async_2.js
+++ 5-async/async_2.js
@@ -6,4 +6,13 @@ const getName = (username)=>{
         .then(response=>response.json())
         .then(data=>console.log(data))
 }
-getName('UsuarioTemporal')
+// getName('UsuarioTemporal')
+
+const asyncGetName= async (username)=>{
+    const URL = `http://api.github.com/users/${username}`
+    const response = await fetch(URL)
+    const data = await response.json()
+    return data
+}
+asyncGetName('UsuarioTemporal').then(response=>console.log(response))
+// console.log(asyncGetName('UsuarioTemporal')); Estp retorna Promise { <pending> }
