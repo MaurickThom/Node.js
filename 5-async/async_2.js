99040d9e595344f551fad1de38f4807be16a130e
UsuarioTemporal <thomtwd@gmail.com>
2019-02-08 01:07:42 -0500

    Multiples formas de usar el async await

--- 5-async/async_2.js
+++ 5-async/async_2.js
@@ -12,7 +12,22 @@ const asyncGetName= async (username)=>{
     const URL = `http://api.github.com/users/${username}`
     const response = await fetch(URL)
     const data = await response.json()
+    if(response.status!==200) throw Error('El usuario no existe')
+    console.log(data.name)
+
     return data
 }
-asyncGetName('UsuarioTemporal').then(response=>console.log(response))
+asyncGetName('UsuarioTemporal')
+    .then(response=>console.log(response))
+    .catch(err=>console.log(err));
 // console.log(asyncGetName('UsuarioTemporal')); Estp retorna Promise { <pending> }
+
+
+(async ()=>{
+    try{
+        const user = await asyncGetName('UsuarioTemporal')
+        console.log(user)
+    }catch(err){
+        console.log(err)
+    }
+})();
