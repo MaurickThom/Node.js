eacbca86ed23352e4617e7a4b1df5c46d1738fe5
UsuarioTemporal <thomtwd@gmail.com>
2019-02-07 21:55:16 -0500

    Promise Hell

--- 3-modelos-asincronicos/promise_hell.js
+++ 3-modelos-asincronicos/promise_hell.js
@@ -4,5 +4,19 @@ const log = console.log.bind(console)
 
 const dummy  = payload =>wait(1000).then(()=>payload)
 const user =()=>dummy({user:'jhon'})
-const tweets = ()=>dummy({tweet:['GuiaJS','NodeJS']})
+const tweets = ()=>dummy({tweet:['noticia_1','noticia_2']})
+const github = ()=>dummy({repos:['GuiaJS','NodeJS']})
+
+const buildResult = (user,tweets,github)=>({user,tweets,github})
+
+const fetchResult = ()=>{
+    return user().then(user=>{
+        return tweets().then(tweet=>{
+            return github().then(rep=>{
+                return buildResult(user,tweet,rep)
+            })
+        })
+    })
+}
 // user().then(log)
+fetchResult().then(log)
