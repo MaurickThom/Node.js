29e5650e9693e5404b07437cf4b1a474917f089e
UsuarioTemporal <thomtwd@gmail.com>
2019-02-07 23:55:54 -0500

    Soy parte de la API de github :''3

--- 5-async/async_2.js
+++ 5-async/async_2.js
@@ -1,3 +1,7 @@
 const getName = (username)=>{
     const URL = `http://api.github.com/users/${username}`
+    fetch(URL)
+        .then(response=>response.json())
+        .then(data=>console.log(data))
 }
+getName('UsuarioTemporal')
