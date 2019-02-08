d8073d551d22915181539a66343c06206659d54d
UsuarioTemporal <thomtwd@gmail.com>
2019-02-07 21:48:09 -0500

    Simulacion de API's

--- 3-modelos-asincronicos/promise_hell.js
+++ 3-modelos-asincronicos/promise_hell.js
@@ -1,3 +1,8 @@
 // Version promise hell
 const wait = numberTime =>new Promise(resolve=>setTimeout(resolve,numberTime))
 const log = console.log.bind(console)
+
+const dummy  = payload =>wait(1000).then(()=>payload)
+const user =()=>dummy({user:'jhon'})
+const tweets = ()=>dummy({tweet:['GuiaJS','NodeJS']})
+// user().then(log)
