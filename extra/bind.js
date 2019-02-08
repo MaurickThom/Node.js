1c2d8aa0de2cf06dd95f77aa4d0691752531abb7
UsuarioTemporal <thomtwd@gmail.com>
2019-02-07 20:55:17 -0500

    Pruebas

--- extra/bind.js
+++ extra/bind.js
@@ -19,7 +19,8 @@ let newFn_1 = fn_1.bind(console)
 let newFn_1_plus = fn_1.bind(console,'Uno','Dos')
 newFn_1('1','2')
 newFn_1_plus('1','2')
+
 let myObject = {
     name:'Thom'
 }
-let otherFun = fn_1.apply(myObject,[1,2])
+// fn_1.apply(myObject)
