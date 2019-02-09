d6bc7c4a14383dfe01b11b7472a343fd8d070d24
UsuarioTemporal <thomtwd@gmail.com>
2019-02-09 01:06:39 -0500

    Correcto 2

---
+++ 4-correcto-vs-incorrecto/correcto_2.js
@@ -0,0 +1,28 @@
+const usuarios = [
+	{
+		id: 1,
+		nombre: 'thom',
+		profesion_id: [1, 7]
+	},
+	{
+		id: 2,
+		nombre: 'carlos',
+		profesion_id: [2, 1]
+	},
+	{
+		id: 3,
+		nombre: 'juan',
+		profesion_id: [1, 3, 2]
+	},
+	{
+		id: 4,
+		nombre: 'Perez',
+		profesion_id: [3, 2]
+	}
+]
+const profesion = {
+	1: 'programador',
+	2: 'diseñador',
+	3: 'Cocinero',
+	4:'Chef'
+}
