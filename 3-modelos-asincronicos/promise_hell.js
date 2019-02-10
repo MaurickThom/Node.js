a95381ad473ce58d28750a53ebe30cbfb68af50b
UsuarioTemporal <thomtwd@gmail.com>
2019-02-09 21:35:35 -0500

    Mejorando el PRomiseHell

--- 3-modelos-asincronicos/promise_hell.js
+++ 3-modelos-asincronicos/promise_hell.js
@@ -1,25 +1,34 @@
-// Version promise hell
-const wait = numberTime =>new Promise(resolve=>setTimeout(resolve(),numberTime))
+const wait = delay => new Promise(resolve=>setTimeout(resolve(),delay))
 const log = console.log.bind(console)
 
-const dummy  = payload =>wait(1000).then(()=>payload)
+const dummy = payload=>wait(1000).then(()=>payload)
 
-const user =()=>dummy({user:'jhon'})
-const tweets = ()=>dummy({tweet:['noticia_1','noticia_2']})
-const github = ()=>dummy({repos:['GuiaJS','NodeJS']})
+const user= ()=>dummy({user:'Thom'})
+const tweets = ()=>dummy({tweets:['notify_1','notify_2']})
+const github = ()=>dummy({repositories:['GuiaJS','NodeJS','Guia_Python']})
 
 const buildResult = (user,tweets,github)=>({user,tweets,github})
 
-const fetchResult = ()=>{
-	return user().then(user=>{
-		return tweets().then(tweet=>{
-			return github().then(rep=>{
-				return buildResult(user,tweet,rep)
-			})
-		})
-	})
-}
-// user().then(log)
-fetchResult().then(log)
 
-//mejor opcion con Promise.all
+// Promise Hell (Aggg xdxd) >:(
+const fetchResult = ()=>{
+	return user()
+			.then(user=>{
+				return tweets()
+						.then(twts=>{
+							return github()
+									.then(rep=>{
+										return buildResult(user,twts,rep)
+									})
+						})
+			})
+}
+// fetchResult()
+// 	.then(log)
+
+// Promise all :)
+const liftPromises = fn =>(...args)=>Promise.all(args).then(args=>fn(...args))
+const fetcResultPromiseLift =()=>liftPromises(buildResult)(user(),tweets(),github())
+fetcResultPromiseLift().then(log)
+
+// ASync Await :3
