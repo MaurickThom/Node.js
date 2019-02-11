c9bdb7ccc3f997e62960996368b424d0b9125bfe
UsuarioTemporal <thomtwd@gmail.com>
2019-02-11 13:44:04 -0500

    Extra

---
+++ extra/buscador.js
@@ -0,0 +1,11 @@
+class Search{
+    constructor(obj){
+        this.el = document.querySelector(obj.el);
+        this.list = obj.list;
+        this.init();
+    }
+    init(){
+        let input , listContainer
+        this.el.innerHTML =``
+    }
+}
