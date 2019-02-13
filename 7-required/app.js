f1d7135dc136b7ba0018a331cabc8ca68b9c5e8c
UsuarioTemporal <thomtwd@gmail.com>
2019-02-12 22:05:37 -0500

    Tres tipos de required

--- 7-required/app.js
+++ 7-required/app.js
@@ -1,3 +1,10 @@
-//Se acostumbra colocar los archivos requeridos al comenzar el código
+//Se acostumbra colocar las librerias requeridos al comenzar el código
 // Node.js contiene archivos por defecto que podemos utilizar sin ningun npm install
 // https://nodejs.org/dist/latest-v10.x/docs/api/fs.html
+
+// Existen 3 tipos de required
+/**
+ * 1. El que es propio de node que no se requiere ningun npm install
+ * 2. Existen otras que se necesitan instalar, Otras que son casi lo mismo que la 2 pero a diferencia esta libreria son desarrolladas por otras entidades
+ * 3. Y la ultima de los required que nosotros mismos creamos
+ */
