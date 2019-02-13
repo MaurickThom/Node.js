1926246b7140fca5daf1bbfdf3a5556c35b180b1
UsuarioTemporal <thomtwd@gmail.com>
2019-02-12 21:45:15 -0500

    Pequeño to-do con Node.js desde ternminal

--- 6-to-do/app.js
+++ 6-to-do/app.js
@@ -1,2 +1,19 @@
 const argv = require('yargs').argv
 console.log(argv)
+
+let command = argv._[0]
+
+switch (command.toLowerCase()) {
+    case 'crear':
+        console.log('Crear nota')    
+        break
+    case 'listar':
+        console.log('Listado')
+        break
+    case 'actualizar' :
+        console.log('Actualizando')
+        break
+    default:
+        console.log('No existe ese comando')
+    
+}
