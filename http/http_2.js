56bcaaedbc8e4e65a421bae3b85135cac632095f
UsuarioTemporal <thomtwd@gmail.com>
2019-02-13 13:06:03 -0500

    Definicion de verbos y headers

--- http/http_2.js
+++ http/http_2.js
@@ -10,4 +10,9 @@ app.listen(3000,()=>{
     console.log('Puerto 3000')
 })
 
-// Verbos
+// Verbos GET POST  :Define el tipo de request de pedido que se hara al servidor
+/**
+ * GET : obtener la ruta / y luego define la version del protocolo a usar (GET / HTTP/1.1)
+ * 
+ * headers : son informacion adicional que el cliente puede darle al servidor
+ */
