dc41448b062867ec6d6059f84f8fc03dd920ee47
UsuarioTemporal <thomtwd@gmail.com>
2019-02-13 13:49:02 -0500

    ARGV

--- 7-required/app_2.js
+++ 7-required/app_2.js
@@ -2,5 +2,12 @@
 // multiplicar.crearArchivo()
 
 const {crearArchivo} = require('./modul')
-crearArchivo('53f').then(response=>console.log(response))
+
+// process - cuando node se ejecuta crea esta variable de entorno
+// console.log(process.argv[2]);
+let argv = process.argv
+let param = argv[2]
+let base = param.split('=')
+
+crearArchivo(base[1]).then(response=>console.log(response))
                 .catch(err=>console.log(err))
