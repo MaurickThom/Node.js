a233e3b3ede5b164909baabb2aebb34b6a02bd7d
UsuarioTemporal <thomtwd@gmail.com>
2019-02-19 14:13:38 -0500

    Persistencia de datos

--- 6-to-do/to.do/to-do.js
+++ 6-to-do/to.do/to-do.js
@@ -1,12 +1,25 @@
 const fs = require('fs')
-
+const colors = require('colors')
 let arrToDo = []
+
+const saveDB = ()=>{
+    return new Promise((resolve,reject)=>{
+        let data =JSON.stringify(arrToDo)
+        if(!data) return reject('Error data is empty')
+        fs.writeFile('db/data.json',data,err=>{
+            if(err) return reject(err)
+            return resolve(data)
+        })
+    })
+}
 const create = (description)=>{
     let toDo = {
         description,
         complete:false
     }
     arrToDo.push(toDo)
+    saveDB().then(result=>console.log(result.green))
+            .catch(err=>console.log(err.red))
     return toDo
 }
 module.exports = {
