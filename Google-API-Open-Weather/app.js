ef68ae2a41f8e05ef9d544a5685050b6afd131e0
UsuarioTemporal <thomtwd@gmail.com>
2019-02-22 11:11:01 -0500

    api key

--- Google-API-Open-Weather/app.js
+++ Google-API-Open-Weather/app.js
@@ -1,8 +1,13 @@
 // Request trabaja en base a callback 
 // Axios trabaja en base a promesas
+const axios = require('axios')
 const argv =require('yargs').options({
     direction:{
         alias:'d',
-        desc:'Dirección de la ciudad'
+        desc:'Dirección de la ciudad',
+        demand:true
     }
-})
+}).help().argv
+console.log(argv.direction);
+let encodeUrl = encodeURI(argv.direction)
+axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI}&key=AIzaSyBKUogpwJD8WOTie2RHGDGshTTFOhGKxiU`)
