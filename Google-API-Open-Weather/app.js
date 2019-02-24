a2baabe496e8716470a7e1072d5d6a2c2880b152
UsuarioTemporal <thomtwd@gmail.com>
2019-02-24 16:06:04 -0500

    Consultando la dirección

--- Google-API-Open-Weather/app.js
+++ Google-API-Open-Weather/app.js
@@ -15,7 +15,13 @@ console.log(argv.direction);
 let encodeUrl = encodeURI(argv.direction)
 axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeUrl}&key=AIzaSyBKUogpwJD8WOTie2RHGDGshTTFOhGKxiU`)
 .then((result) => {
-    console.log(result);
+    console.log(result.data);
+    let location = result.data.results[0]
+    let coords = location.geometry.location
+
+    console.log('Dirección : ',location.formatted_address)
+    console.log('Lat : ',coords.lat);
+    console.log('Lng : ',coords.lng);
 }).catch((err) => {
     console.log("Error");
 });
