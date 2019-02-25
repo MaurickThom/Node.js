6ffa268ce06606db8f48c3c671368b8b27065edf
UsuarioTemporal <thomtwd@gmail.com>
2019-02-24 19:40:24 -0500

    Corregido

--- Google-API-Open-Weather/lugar/lugar.js
+++ Google-API-Open-Weather/lugar/lugar.js
@@ -3,9 +3,10 @@ const lugarLatLng = async(direction)=>{
 
     let encodeUrl = encodeURI(direction)
     const resp = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeUrl}&key=AIzaSyBKUogpwJD8WOTie2RHGDGshTTFOhGKxiU`)
-    if ( resp.data.status='ZERO_RESULTS') throw new Error('No hay resultados')
+    let result = resp.data
+    if ( result.status==='ZERO_RESULTS' || result.status==='OVER_QUERY_LIMIT') throw new Error('No hay resultados')
 
-    let location = result.data.results[0]
+    let location = result.results[0]
     let coords = location.geometry.location
     return {
         direc :location.formatted_address,
