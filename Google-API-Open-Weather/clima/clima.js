a0e00a5c67e614766078cca8656c786f03e63234
UsuarioTemporal <thomtwd@gmail.com>
2019-02-24 20:33:34 -0500

    metodo get

--- Google-API-Open-Weather/clima/clima.js
+++ Google-API-Open-Weather/clima/clima.js
@@ -1,1 +1,9 @@
 // https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=3f87578f9f45a2e27f142c9f65e17335
+const axios = require('axios')
+const getClima = async (lat,long)=>{
+    // return 
+    const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=3f87578f9f45a2e27f142c9f65e17335`)
+}
+module.exports = {
+    getClima
+}
