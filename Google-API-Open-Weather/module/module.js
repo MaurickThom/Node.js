9adf6606bc099a90f3e225b265eb860c97b3e3fd
UsuarioTemporal <thomtwd@gmail.com>
2019-02-24 21:05:06 -0500

    Terminado

---
+++ Google-API-Open-Weather/module/module.js
@@ -0,0 +1,14 @@
+const {lugarLatLng} = require('./lugar/lugar')
+const {getClima} = require('./clima/clima')
+
+const getCoordsOf = async(direction)=>{
+    const resp = await lugarLatLng(direction)
+    const data  = await getClima(resp.lat,resp.long)
+    return{
+        country : resp.direct,
+        data
+    }
+}
+module.exports ={
+    getCoordsOf
+}
