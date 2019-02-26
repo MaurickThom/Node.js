793b4a7fead51dd3a3794074d7b896fa02f8a144
UsuarioTemporal <thomtwd@gmail.com>
2019-02-25 20:45:57 -0500

    Enviando JSON con express

--- WebServer-Express/app-express.js
+++ WebServer-Express/app-express.js
@@ -2,7 +2,14 @@ const express = require('express')
 
 const app = express()
 
-app.get('/',(request,response)=>{ // Ese / significa que toda las peticiones que netren por esa barra ejecutaran el callbock
-    response.send('Hola mundo')
+app.get('/',(request,response)=>{ // Ese / significa que toda las peticiones que entren por esa barra ejecutaran el callbock
+    // response.send('Hola mundo!!')
+    let salida = {
+        nombre:'Thom',
+        edad:20,
+        url :request.url
+    }
+    response.send(salida)
+
 })
 app.listen(8080)
