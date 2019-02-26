6c8aa86ccd86b7d7c08ae0116e06be33e483d4fa
UsuarioTemporal <thomtwd@gmail.com>
2019-02-25 19:24:19 -0500

    Enviado json al browser

--- WebServer-Express/app.js
+++ WebServer-Express/app.js
@@ -2,5 +2,18 @@ const http = require('http')
 
 // creando un servidor 
 http.createServer((request,response)=>{
-    response.write('Hola mundo')
+    response.writeHead(200,{
+        'Content-type':'application/json'
+    })
+    let salida = {
+        nombre:'Thom',
+        edad:20,
+        url :request.url
+    }
+    // response.write('Hola mundo')
+    response.write(JSON.stringify(salida))
+    response.end()
 })
+.listen(8080)
+
+console.log('Escuchando el puerto 8080');
