5f15739e94804be7bd7b5d765c1d127b56cac95b
UsuarioTemporal <thomtwd@gmail.com>
2019-02-25 20:32:44 -0500

    Explicación de express

--- WebServer-Express/app-express.js
+++ WebServer-Express/app-express.js
@@ -1,1 +1,8 @@
 const express = require('express')
+
+const app = express()
+
+app.get('/',(request,response)=>{ // Ese / significa que toda las peticiones que netren por esa barra ejecutaran el callbock
+    response.send('Hola mundo')
+})
+app.listen(8080)
