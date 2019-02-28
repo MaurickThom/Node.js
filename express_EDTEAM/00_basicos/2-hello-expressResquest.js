cde832fbe8e3cc4724d2b9f9e45e04637203df21
UsuarioTemporal <thomtwd@gmail.com>
2019-02-28 16:05:50 -0500

    variables en la URL

--- express_EDTEAM/00_basicos/2-hello-expressResquest.js
+++ express_EDTEAM/00_basicos/2-hello-expressResquest.js
@@ -1,28 +1,40 @@
 const express = require('express'),
     app = express()
 
-app.get('/',(request,response)=>response.end('<h1>sdfsdf</h1>'))
-    .get('/users',(request,response)=>{
+app.get('/', (request, response) => response.end('<h1>sdfsdf</h1>'))
+    .get('/users', (request, response) => {
         response.set({
-            'content-type':'text/html;charset=utf-8'
-        })
-        response.end(`
+                'content-type': 'text/html;charset=utf-8'
+            })
+            .end(`
             <p>
                 Sección de usuarios
             </p>
         `)
     })
-    .get('/users/:id-:name-:age',(request,response)=>{
-        response.end(`
+    .get('/users/:id-:name-:age', (request, response) => {
+        response
+            .set({
+                'content-type': 'text/html;charset=utf-8'
+            })
+            .end(`
             <h1>Hola ${request.params.name} bienvenido a Express.js</h1>
             <p>Tu id de usuario es <b>${request.params.id}</b></p>
             <p>Tu edad de usuario es <b>${request.params.age}</b></p>
 
         `)
     })
-    .get('/search',(request,response)=>{
+    .get('/search', (request, response) => {
+        // /search?s=Una+busqueda
 
+        response
+            .set({
+                'content-type': 'text/html;charset=utf-8'
+            })
+            .end(`
+                <h1>Hola ${request.query.s} busqueda Express.js</h1>
+            `)
     })
-.listen(3000,()=>{
-    console.log('Puerto 3000');
-})
+    .listen(3000, () => {
+        console.log('Puerto 3000');
+    })
