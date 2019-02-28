8167dcb8d355da6865fcb3dc0ea178562de93767
UsuarioTemporal <thomtwd@gmail.com>
2019-02-28 14:06:41 -0500

    Nuevos cambios

---
+++ express_EDTEAM/00_basicos/01-hello-express-fs.js
@@ -0,0 +1,9 @@
+const express = require('express'),
+    app = express()
+
+app.get('/',(request,response)=>{
+    response.sendFile(`${__dirname}/index.html`)// la variable __dirname nos representa el directorio de donde se esta ejecutando el archivo que esta levantando nodejs
+})
+.listen(3000,()=>{
+    console.log('Iniciando Express.js en el puerto 3000');
+}) 
