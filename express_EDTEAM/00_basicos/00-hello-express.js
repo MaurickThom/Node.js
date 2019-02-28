b074ef20b1fc76a6e5196c819fe2a9b0e673655c
UsuarioTemporal <thomtwd@gmail.com>
2019-02-28 00:22:48 -0500

    Avance 2

--- express_EDTEAM/00_basicos/00-hello-express.js
+++ express_EDTEAM/00_basicos/00-hello-express.js
@@ -1,10 +1,9 @@
-const express = require('express')
-
-const app = express()
+const express = require('express'),
+    app = express()
 
 app.get('/',(request,response)=>{
     response.end('<h1>HOLA MUNDO DESDE EXPRESS</h1>')
 })
 .listen(3000,()=>{
     console.log('Iniciando Express.js en el puerto 3000');
-})
+}) 
