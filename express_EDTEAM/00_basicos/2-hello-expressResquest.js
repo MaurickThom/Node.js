70c7f66523f5a9b3c899a353f74cb7940f6d69f4
UsuarioTemporal <thomtwd@gmail.com>
2019-02-28 15:28:27 -0500

    Rutas con parametros

--- express_EDTEAM/00_basicos/2-hello-expressResquest.js
+++ express_EDTEAM/00_basicos/2-hello-expressResquest.js
@@ -1,13 +1,28 @@
 const express = require('express'),
     app = express()
 
-app.get('/',(request,response)=>response.end('<h1></h1>'))
+app.get('/',(request,response)=>response.end('<h1>sdfsdf</h1>'))
+    .get('/users',(request,response)=>{
+        response.set({
+            'content-type':'text/html;charset=utf-8'
+        })
+        response.end(`
+            <p>
+                Sección de usuarios
+            </p>
+        `)
+    })
     .get('/users/:id-:name-:age',(request,response)=>{
         response.end(`
             <h1>Hola ${request.params.name} bienvenido a Express.js</h1>
+            <p>Tu id de usuario es <b>${request.params.id}</b></p>
+            <p>Tu edad de usuario es <b>${request.params.age}</b></p>
+
         `)
     })
-    .get('',(request,response)=>{
+    .get('/search',(request,response)=>{
 
     })
-
+.listen(3000,()=>{
+    console.log('Puerto 3000');
+})
