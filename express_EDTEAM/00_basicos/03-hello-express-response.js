415a863ba56e707dbddaaeb067f54d5caac171de
UsuarioTemporal <thomtwd@gmail.com>
2019-03-01 11:17:24 -0500

    Renderizados

--- express_EDTEAM/00_basicos/03-hello-express-response.js
+++ express_EDTEAM/00_basicos/03-hello-express-response.js
@@ -4,12 +4,22 @@ const express = require('express'),
 
 app
 .get('/',(request,response)=>{
-    response.send('')
+    response.send('adasdasd')
 })
 .get('/escuela-digital',(request,response)=>{
-    response.send(html`
-        <h1>Hola Escuel aDigital</h1>
-    `)
+    // response.send(html`
+    //     <h1>Hola Escuel aDigital</h1>
+    // `)
+    response.redirect(301,"https://ed.team")
+})
+.get('/json',(request,response)=>{
+    response.json({
+        name:'Thom',
+        age:20
+    })
+})
+.get('/render',(request,response)=>{
+    // response.render(`${__dirname}/index.html`) permite solo trabajar con algun sistema template
 })
 .get('/',(request,response)=>{})
-.get('/',(request,response)=>{})
+.listen(3000)
