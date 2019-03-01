45c50d3488033d0b7225b1841e7196a72cfb4a2d
UsuarioTemporal <thomtwd@gmail.com>
2019-03-01 10:17:55 -0500

    Configurando el objeto response

---
+++ express_EDTEAM/00_basicos/03-hello-express-response.js
@@ -0,0 +1,11 @@
+const express = require('express'),
+    app = express()
+
+
+app
+.get('/',(request,response)={
+    response.send
+})
+.get('/',(request,response)={})
+.get('/',(request,response)={})
+.get('/',(request,response)={})
