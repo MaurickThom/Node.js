8fa7ab0feba541c08bf2b21725a245daeb2c15c8
UsuarioTemporal <thomtwd@gmail.com>
2019-03-02 17:37:26 -0500

    Cambios con handlebars

--- WebServer-Express/server-template.js
+++ WebServer-Express/server-template.js
@@ -2,6 +2,11 @@ const express = require('express'),
     app = express()
 
 app.use(express.static(`${__dirname}/public`))
-app.set('view engine','pug')
+app.set('view engine','hbs')
 // app.set('view engine','hbs')
+.get('/',(request,response)=>{
+    response.render('home',{
+        year:new Date().getFullYear()
+    })
+})
 .listen(3000)
