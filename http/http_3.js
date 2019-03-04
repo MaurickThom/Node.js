ff2c6f20c0972a0b2f091a69470452cd53d98de8
UsuarioTemporal <thomtwd@gmail.com>
2019-03-04 18:41:40 -0500

    Minimalismo express

--- http/http_3.js
+++ http/http_3.js
@@ -11,6 +11,10 @@ app.set('views',`${__dirname}/views`)
 .get('/encuesta',(request,response)=>{
     response.render('encuesta')
 })
+.post('/responder_encuesta',(request,response)=>{
+    console.log(request.body); // express es muy minimalista, es decir no trae nada que no necesitas , nisiquiera trae al procesar el body de los request
+    response.send('Gracias')
+})
 .listen(3000,()=>{
 
-})
+}) 
