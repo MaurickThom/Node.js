02c9f589a20de06af1ebdfa99f20a11e06b6f53b
UsuarioTemporal <thomtwd@gmail.com>
2019-03-06 21:35:08 -0500

    Terminado

--- WebServer-Express-hbs/server-template.js
+++ WebServer-Express-hbs/server-template.js
@@ -1,14 +1,18 @@
 const express = require('express'),
     app = express(),
-    hbs = require('hbs')
+    hbs = require('hbs'),
+    port = process.env.PORT || 3000
 // los parciales son un bloque de código html que nosostros podemos reutilizar
 app.use(express.static(`${__dirname}/public`))
 hbs.registerPartials(`${__dirname}/views/parcials`)
+
+// un helpers no es mas que una funcion que se dispara cuando el template lo require
+
+hbs.registerHelper('year',()=>new Date().getFullYear())
 app.set('view engine','hbs')
 // app.set('view engine','pug')
 .get('/',(request,response)=>{
     response.render('home',{
-        year:new Date().getFullYear(),
         current:'Home'
     })
 })
@@ -17,4 +21,4 @@ app.set('view engine','hbs')
         current:'About'
     })
 })
-.listen(3000)
+.listen(port)
