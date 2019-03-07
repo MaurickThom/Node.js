033cd76f0915e62f6f9a08efe87314d8fbd4f76e
UsuarioTemporal <thomtwd@gmail.com>
2019-03-06 22:46:20 -0500

    Practicando

---
+++ WebServer-Express-pug/server.js
@@ -0,0 +1,9 @@
+const express = require('express'),
+    app = express(),
+    port = process.env.PORT || 3000
+app.use(express.static(`${__dirname}/public`))
+
+app.set('view engine','pug')
+.get('/',(request,response)=>{
+    
+})
