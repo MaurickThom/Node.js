e55705123bc9061511ff03c51b4634ce84ef3f65
UsuarioTemporal <thomtwd@gmail.com>
2019-03-07 15:03:25 -0500

    Nuevas dependencias

--- express_EDTEAM/01_Avanzado/server.js
+++ express_EDTEAM/01_Avanzado/server.js
@@ -7,3 +7,4 @@ app.listen(app.get('port'),()=>{
 })
 // corriendo el servidor con npm start server
 // morgan es como un logger que me permite monitoriar las peticiones de mi aplicación
+// $ npm i cookie-parse express-session morgan hbs node-sass-middleware --save
