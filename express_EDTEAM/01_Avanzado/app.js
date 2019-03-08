2fe00cac9ac69818d0c3d4470d56d68395f0c706
UsuarioTemporal <thomtwd@gmail.com>
2019-03-08 01:28:16 -0500

    Insertando middleware

--- express_EDTEAM/01_Avanzado/app.js
+++ express_EDTEAM/01_Avanzado/app.js
@@ -6,9 +6,26 @@ const express = require('express'),
     session=require('express-session'),
     sassMiddleware = require('node-sass-middleware'),
     browserify_express = require('babelify-express'),
+    hbs  = require('hbs'),
+    hbsHelpers = require('./views/helpers'),
+    routes = require('./routes/index'),
     app = express()
 
 app
     .set('port', (process.env.PORT || 3000))
-
+    .set('views',`${__dirname}/views`)
+    .set('view_engine','hbs')
+    .use(favicon(`${__dirname}/public/img/favicon.io`))
+    .use(logger('dev'))
+    .use(express.json())
+    .use(express.urlencoded({extended:false}))
+    .use(cookieParser())
+    .use(session({
+        secret:'ESTO ES SECRETO',
+        resave:true,
+        saveUninitialized:true
+    }))
+    .use(sassMiddleware({
+        src:''
+    }))
 module.exports = app
