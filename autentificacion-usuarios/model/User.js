694de0ea6468f467695e221c870de054f5e52d29
UsuarioTemporal <thomtwd@gmail.com>
2019-03-12 18:53:18 -0500

    Creando atributos de la base de datos en Mongo

--- autentificacion-usuarios/model/User.js
+++ autentificacion-usuarios/model/User.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose'),
-    Schema = mongoose.Schema, // requesenta la información y las propiedades que van a estar en un objeto de tipo user
+    Schema = mongoose.Schema, // representa la información y las propiedades que van a estar en un objeto de tipo user
     userSchema = new Schema({
-
+        email:{type:String,unique:true,lowercase:true,required:true},
+        password : {type:String,required:true},
+        name :{type:String,required:true}
+    },{
+        timestamps:true // mongoose agreagar atributros aparte de los que hemos declarado a nuestra base de datos en las cuales indicara cuando el usuario fue creado y cuando fue actualizado
     })
