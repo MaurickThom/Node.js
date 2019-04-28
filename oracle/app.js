255e73b216adeb8f545a30baabc421592821eec3
UsuarioTemporal <thomtwd@gmail.com>
2019-04-27 19:20:54 -0500

    Creacion de la funcion doRelease
    Esta funcion es para cerrar la connecion con la base de datos ORACLE

--- oracle/app.js
+++ oracle/app.js
@@ -1,12 +1,14 @@
 let oracledb = require('oracledb')
 oracledb.getConnection({
     user:'HR',
-    password:'HR'
-}),
+    password:'HR',
+    port:'1521',
+    connectString:'localhost/xe'
+},
 (err,connection)=>{
     if(err) return console.log(err.message)
-    connection.execute('SELECT * FROM employees'),
-    [101],
+    connection.execute('SELECT * FROM employees',
+    [],
     {
         maxRows:1
     },
@@ -18,5 +20,11 @@ oracledb.getConnection({
         }
         console.log(result.now)
         doRelease(connection)
-    }
+    });
+});
+
+const doRelease = connection=>{
+    connection.close(err=>{
+        if(err) return console.log(err.message)
+    });
 }
