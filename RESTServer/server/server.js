a5f3bc5d5303812cfbe028be865d00154f0aba41
UsuarioTemporal <thomtwd@gmail.com>
2019-05-24 16:37:35 -0500

    Verbos HTTP , repaso

--- RESTServer/server/server.js
+++ RESTServer/server/server.js
@@ -3,6 +3,15 @@ const express = require('express'),
     port = process.env.PORT || 8000
 
 app.get('/',(request,response)=>{
-    
+    response.send('hola')
+})
+.get('/usuario',(req,res)=>{
+    res.json('Get user')
+})
+.post('/usuario',(req,res)=>{
+    res.json('post usuarios')
+})
+.put('/usuario',(req,res)=>{
+    res.json('Put user')
 })
 .listen(port,()=>console.log(`Escuchando en el puerto ${port}`))
