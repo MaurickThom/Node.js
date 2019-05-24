4e3560337d804615ad7fd2a844c09508973c2a7c
UsuarioTemporal <thomtwd@gmail.com>
2019-05-23 22:15:07 -0500

    Repasando multiples peticiones

--- repasoNode/server.js
+++ repasoNode/server.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express()
 
 app.get('/',(req,res)=>{
-    res.send('Segunda prueba express')
+    let salida = {
+        name:'Thom',
+        url:req.url
+    }
+    // res.json(salida)
+    // res.send('Segunda prueba express')
+    res.send(JSON.stringify(salida))
+})
+.get('/data',(req,res)=>{
+    res.send('Otra cosa')
 })
 .listen(8000,()=>console.log('Escuchando en el puerto 8000'))
