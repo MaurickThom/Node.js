c0a1bf81f1e025c4a432b23f4bb29e8336d7a105
UsuarioTemporal <thomtwd@gmail.com>
2019-05-24 21:36:59 -0500

    STATUS code

--- RESTServer/server/server.js
+++ RESTServer/server/server.js
@@ -2,16 +2,24 @@ const express = require('express'),
     app = express(),
     port = process.env.PORT || 8000
 
+app.use(express.urlencoded({extended:false}))
 app.use(express.json())
-.use(express.urlencoded({extended:false}))
 .get('/',(request,response)=>{
     response.send('hola')
 })
 .get('/usuario',(req,res)=>{
+    
     res.json('Get user')
 })
 .post('/usuario',(req,res)=>{
-    res.json('post usuarios')
+    let body = req.body
+    if(body.nombre ===undefined) {
+        res.status(2)
+        return
+    }
+    res.json({
+        body
+    })
 })
 .put('/usuario/:id',(req,res)=>{
     res.json('Put user '+req.params.id)
