ec82b1c39a1e22b265d799d3c11b513a3e45b25b
MaurickThom <thomtwd@gmail.com>
2019-07-12 20:08:08 -0500

    [utilizando socket]

--- socketio/server/index.js
+++ socketio/server/index.js
@@ -3,6 +3,10 @@ const express = require('express'),
     server = require('http').Server(app),
     io = require('socket.io')(server)
 
+app.get('/',(req,res)=>{
+    res.send('me la pela')
+})
+
 server.listen(8000,()=>console.log('Escuchando en el puerto 8000'))
 
 
